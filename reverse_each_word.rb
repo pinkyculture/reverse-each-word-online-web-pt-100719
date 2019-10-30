f7a2dd3b1a3d1d092957ea31e6504af403a1739c
daniel arzayus <randomblue1@yahoo.com>
2019-10-30 04:50:49 +0000

    Automatically backed up by Learn

---
+++ reverse_each_word.rb
@@ -0,0 +1,8 @@
+def reverse_each_word(hello)
+  original_array = string.split(" ")
+  return_array = []
+  original_array.each do|string|
+    return_array << string.reverse
+  end
+  return_array.join(" ")
+end
